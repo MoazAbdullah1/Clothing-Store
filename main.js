eab9b51276f88f65149773a107c91724eb4f1805
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-08-21 19:06:20 +0500

    Main Js Edit

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, price, desc, img } = x;
+
       let search = basket.find((x) => x.id === id) || [];
+
       return `
     <div id=product-id-${id} class="item">
+
         <img width="220" src=${img} alt="">
+        
         <div class="details">
           <h3>${name}</h3>
           <p>${desc}</p>
